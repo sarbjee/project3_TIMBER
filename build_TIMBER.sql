36885f89c4b10e4d010121bc9a964893d294f7d3
sarbjeet <sarbjeetb2@gmail.com>
2023-03-11 19:42:48 -0700

    order_items column added to access number of items in order

--- build_TIMBER.sql
+++ build_TIMBER.sql
@@ -10,7 +10,7 @@ DROP TABLE TIM_category cascade constraints;
 DROP TABLE TIM_customer cascade constraints;
 -- create tables
 CREATE TABLE TIM_customer(
-    customer_number NUMBER CONSTRAINT SYS_CUST_PK PRIMARY KEY,
+    customer# NUMBER CONSTRAINT SYS_CUST_PK PRIMARY KEY,
     customer_street_address VARCHAR2(20) NOT NULL,
     customer_prov CHAR(2) NOT NULL
         CONSTRAINT SYS_CUP_CK CHECK(customer_prov IN('AB','BC','MB','NS','NT','ON','PE','QC','SK','YT','NL','NB')),
@@ -29,10 +29,50 @@ CREATE TABLE TIM_order(
     order_delivery_date DATE NOT NULL,
     shipping_amount NUMBER(10,2),
     tax_amount NUMBER(10,2),
+    order_items NUMBER NOT NULL
+        CONSTRAINT SYS_ITEMS_CK CHECK(order_items>0),
     shipping_prov CHAR(2) NOT NULL
         CONSTRAINT SYS_O_CK CHECK(shipping_prov IN('AB','BC','MB','NS','NT','ON','PE','QC','SK','YT','NL','NB')),
     customer_number NUMBER);
 ALTER TABLE TIM_order
-    ADD CONSTRAINT SYS_O_FK FOREIGN KEY(customer_number) REFERENCES TIM_customer(customer_number);
-    
+    ADD CONSTRAINT SYS_O_FK FOREIGN KEY(customer_number) REFERENCES TIM_customer(customer#)
+    ADD CONSTRAINT sys_OR_PK PRIMARY KEY(order#);
+CREATE TABLE TIM_category(
+    category# NUMBER CONSTRAINT SYS_CAT_PK PRIMARY KEY,
+    category_description VARCHAR2(50) NOT NULL,
+    category_parent# NUMBER);   
+CREATE TABLE TIM_supplier(
+    supplier# NUMBER CONSTRAINT SYS_SUPP_PK PRIMARY KEY,
+    supplier_email VARCHAR2(50) NOT NULL
+        CONSTRAINT SYS_S_CK CHECK(regexp_like(supplier_email,'^(?:(?!.*?[.]{2})[a-zA-Z0-9](?:[a-zA-Z0-9.+!%-]{1,64}|)|\"[a-zA-Z0-9.+!% -]{1,64}\")@[a-zA-Z0-9][a-zA-Z0-9.-]+(.[a-z]{2,}|.[0-9]{1,})$')),
+    CONSTRAINT SYS_SE_UK UNIQUE(supplier_email),
+    supplier_prov CHAR(2) NOT NULL
+        CONSTRAINT SYS_SUPP_CK CHECK(supplier_prov IN('AB','BC','MB','NS','NT','ON','PE','QC','SK','YT','NL','NB')));
+CREATE TABLE TIM_product(
+    product# NUMBER CONSTRAINT SYS_PRO_PK PRIMARY KEY,
+    product_title VARCHAR2(50) NOT NULL,
+    product_description VARCHAR2(100) NOT NULL,
+    product_price NUMBER(10,2) NOT NULL
+        CONSTRAINT SYS_PP_CK CHECK(product_price > 0),
+    product_weight NUMBER(10,2) NOT NULL
+        CONSTRAINT SYS_PW_CK CHECK(product_weight > 0),
+    tex_exempt CHAR(1) NOT NULL
+        CONSTRAINT SYS_TE_CK CHECK(tex_exempt IN (0,1)),
+    category# NUMBER CONSTRAINT SYS_PC_PK REFERENCES TIM_category(category#),
+    order# NUMBER CONSTRAINT SYS_ORDER_PK REFERENCES TIM_order(order#));
+CREATE TABLE TIM_product_supplier(
+    supplier# NUMBER CONSTRAINT SYS_SPS_PK REFERENCES TIM_supplier(supplier#),
+    product# NUMBER CONSTRAINT SYS_PPS_PK REFERENCES TIM_product(product#),
+    quantity_on_hand NUMBER NOT NULL,
+    days_delivery NUMBER NOT NULL);
+ALTER TABLE TIM_product_supplier
+    ADD CONSTRAINT SYS_SSPP_PK PRIMARY KEY(supplier#,product#);
+CREATE TABLE TIM_product_review(
+    review# NUMBER CONSTRAINT SYS_REV_PK PRIMARY KEY,
+    rating NUMBER NOT NULL
+        CONSTRAINT SYS_RATING_CK CHECK(rating between 1 and 5),
+    comments VARCHAR2(100),
+    review_date DATE NOT NULL,
+    product# NUMBER CONSTRAINT SYS_PPV_PK REFERENCES TIM_product(product#),
+    customer# NUMBER CONSTRAINT SYS_CPR_PK REFERENCES TIM_customer(customer#));
 spool off
